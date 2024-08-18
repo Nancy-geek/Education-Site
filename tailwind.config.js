06a9eb8dd95b9bfb82333c3fb8d64bd1db85f6b4
Nancy Gupta <nancynishu2305@gmail.com>
2024-08-18 23:00:09 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  // content: ["./learning/index.html"],
+  content: ["./learning/*.html"],           // * for every index file in learning folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
